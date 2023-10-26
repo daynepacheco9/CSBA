f9748bf68713353b31324ab9294d11c6ce1c4e3b
Dayne Pacheco <143220608+daynepacheco9@users.noreply.github.com>
2023-10-25 21:58:06 -0300

    atualizando cod TDE, ainda não ta pronto

--- matriz.py
+++ matriz.py
@@ -2,105 +2,91 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
+class Matriz:
+    def __init__(self):
+        self.dados = [[0,0,0],[0,0,0],[0,0,0]]
+
+    def setValor(self, lin, col, valor):
+        self.dados[lin][col] = valor
+
+    def setcalcularMaior(self):
+        maior = 0
+        for i in range(3):
+            for j in range(3):
+                if self.dados[i][j] > maior:
+                    maior = self.dados[i][j]
+        return maior 
+
+    def setcalcularMenor(self):
+        menor = 0
+        for i in range(3):
+            for j in range(3):
+                if self.dados[i][j] != 0:
+                    if self.dados[i][j] > menor:
+                        menor = self.dados[i][j]
+        return menor
+    def setsomaDiagonal(self):
+        soma = 0
+        for i in range(3):
+            for j in range(3):
+                if i == j:
+                    soma += self.dados[i][j]
+        return soma
+    
+    def setcalcularSoma(self):
+        somaP = 0
+        for i in range(3):
+            for j in range(3):
+                soma += self.dados[i][j]
+        return somaP
+    
+    def setcalcularProduto(self):
+        produto = 1
+        for i in range(3):
+            for j in range(3):
+                if self.dados[i][j] != 0:
+                    produto *= self.dados[i][j]
+        return produto
+
 class Tela(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.minha_matriz = Matriz()
+
+    def validar(self, tipoValor):
+        try:
+            value = int(tipoValor)
+        except:
+            tipoValor = ''
+            value = 0
+        return value
+
+    def definir_valor(self, lin, col, campo):
+        dado = self.ids[campo].text
+        dadoInt = self.validar(dado)
+
+        self.minha_matriz.setValor(lin,col,dadoInt)
 
     def tipos(self,tipo):
         
-        type1 = self.ids['q1'].text
-        try:
-            value1 = int(type1)                
-        except:
-            self.ids['q1'].text = ''
-            value1 = 0
-
-        type2 = self.ids['q2'].text
-        try:
-            value2 = int(type2)                
-        except:
-            self.ids['q2'].text = ''
-            value2 = 0
-
-        type3 = self.ids['q3'].text
-        try:
-            value3 = int(type3)                
-        except:
-            self.ids['q3'].text = ''
-            value3 = 0
-
-        type4 = self.ids['q4'].text
-        try:
-            value4 = int(type4)                
-        except:
-            self.ids['q4'].text = ''
-            value4 = 0
-
-        type5 = self.ids['q5'].text
-        try:
-            value5 = int(type5)                
-        except:
-            self.ids['q5'].text = ''
-            value5 = 0
-
-        type6 = self.ids['q6'].text
-        try:
-            value6 = int(type6)                
-        except:
-            self.ids['q6'].text = ''
-            value6 = 0
-
-        type7 = self.ids['q7'].text
-        try:
-            value7 = int(type7)                
-        except:
-            self.ids['q7'].text = ''
-            value7 = 0
-
-        type8 = self.ids['q8'].text
-        try:
-            value8 = int(type8)                
-        except:
-            self.ids['q8'].text = ''
-            value8 = 0
-
-        type9 = self.ids['q9'].text
-        try:
-            value9 = int(type9)                
-        except:
-            self.ids['q9'].text = ''
-            value9 = 0
-        
-        lista = [value1, value2, value3, value4, value5, value6,value7,value8,value9]
-
         if tipo == 'MAIOR VALOR':
-            maior = lista[0]
-            for i in range(len(lista)):
-                if lista[i] > maior:
-                    maior = lista[i]
+            maior = self.minha_matriz.setcalcularMaior()
             self.ids.valor.text = str(maior)
             
         elif tipo == 'MENOR VALOR':
-            menor = lista[0]
-            for i in range(len(lista)):
-                if lista[i] < menor:
-                    menor = lista[i]
+            menor = self.minha_matriz.setcalcularMaior()
             self.ids.valor.text = str(menor)
 
-
         elif tipo == 'SOMA DOS ELEMENTOS':
-            soma_total = 0
-            for i in range(len(lista)):
-                soma_total += lista[i]
-            self.ids.valor.text = str(soma_total)
+            soma = self.minha_matriz.setcalcularSoma()
+            self.ids.valor.text = str(soma)
 
         elif tipo == 'SOMA NA DIAGONAL':
-            self.ids.nome.text = 'O valor da compra de' + self.ids['tx2'].text
-            self.ids.mensagem.text = 'Escolhi CRÉDITO 2X' 
+            somaP = self.minha_matriz.setcalcularSoma()
+            self.ids.valor.text = str(somaP)
         
         elif tipo == 'PRODUTO DOS ELEMENTOS':
-            produto = 1
-            for i in range(len(lista)):
-                if lista[i] != 0:
-                    produto *= lista[i]
+            produto = self.minha_matriz.setcalcularProduto()
             self.ids.valor.text = str(produto) 
 
 
@@ -115,17 +101,18 @@ class Tela(BoxLayout):
         self.ids['q7'].text = ''
         self.ids['q8'].text = ''
         self.ids['q9'].text = ''
+        self.ids.mensagem.text = ''
         
 
     def sair(self):
         App.get_running_app().stop()
         Window.close()
 
-class Matriz(App):
+class MatrizApp(App):
     def build(self):
         
         return Tela()
 
 
-Matriz().run()
+MatrizApp().run()
     
